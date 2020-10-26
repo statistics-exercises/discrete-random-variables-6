fd8b67a0653ece0aac8a613da20d0c2b13e025e8
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-26 18:36:21 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# You may wish to write some code here
+
+def binomial(n, p) :
+  # Your code for generating a binomial random variable goes here
+  
+# Your code for generating the list of binomial random variables goes here
+num, prob = 5, 0.5
+  
+# This will plot a graph showing your Bernoulli random variables.
+plt.plot( indices, random_variables, ".") 
+plt.savefig("binomial_rvs.png")
